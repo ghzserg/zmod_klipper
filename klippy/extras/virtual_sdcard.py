ff2deae8823606de3f3460b8ff8e07824aab43f2
Sergei Rozhkov <zmod@zmod.link>
2026-08-05 17:56:44 +0500

    Update

--- klippy/extras/virtual_sdcard.py
+++ klippy/extras/virtual_sdcard.py
@@ -5,7 +5,7 @@
 # This file may be distributed under the terms of the GNU GPLv3 license.
 import os, sys, logging, io
 
-VALID_GCODE_EXTS = ['gcode', 'g', 'gco']
+VALID_GCODE_EXTS = ['gcode', 'g', 'gco', 'gx']
 
 DEFAULT_ERROR_GCODE = """
 {% if 'heaters' in printer %}
